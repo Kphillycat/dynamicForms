ca1655544f670cadf21e5803d0fdb0050ddee47b
Keith Williams Jr <keithwilliamsjr@gmail.com>
2015-06-26 13:47:00 -0400

    Refactor into templates

--- js/directives/myform_directive.js
+++ js/directives/myform_directive.js
@@ -9,26 +9,15 @@
     function myFormDirective($sce){
         return {
             restrict: 'E',
-            template: "",
+            template: "<ng-include src='template'/>",
             scope: {
                 type: "@"
             },
             link: function(scope, element, attrs) {
-                    switch (attrs.type) {
-                        case "select":
-                            element.html($sce.trustAsHtml("<select><option value='One'>One</option></select> <br /> <br />"));
-                            break;
-                        case "file":
-                            element.html($sce.trustAsHtml("<input type=file /> <br /> <br />"));
-                            break;
-                        case "text":
-                        default:
-                            element.html($sce.trustAsHtml("<input type=text /> <br /> <br />"));
-                            break;
-                    }
-                    element.on('click', function(){
-                        element.addClass('Linked');
-                        angular.element(this).find('input').css('background','blue');
+                    scope.template = "templates/pages/inputs/" + attrs.type + ".html";
+                    element.on('click', function(e){
+                        element.addClass('Clicked');
+                        console.log('You clicked on ' + e.target.nodeName);
                     })
             }
         }
